387a26449f2ea4155ab0b7565ff3410bcc6f1449
FARS <francisanielaschettino@gmail.com>
2023-11-08 11:13:09 -0400

    primer commit

---
+++ Exportar.cs
@@ -0,0 +1,117 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.Drawing;
+
+namespace GeneradorBingo
+{
+    public static class Exportar
+    {
+        public static void CrearArchivo(string[][] carton, int n)
+        {
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // Crear un nuevo archivo Excel
+            using (var package = new ExcelPackage(new FileInfo("C:\\Users\\elrey\\OneDrive\\Escritorio\\cartonesBingo.xlsx")))
+            {
+                var workbook = package.Workbook;
+                // Obtener una hoja
+                var sourceWorksheet = workbook.Worksheets["CARTON BASE"];
+                var newWorksheet = workbook.Worksheets.Add($"CARTON{n:D3}");
+
+                // Copia el contenido y formato de la hoja completa
+                foreach (var cell in sourceWorksheet.Cells)
+                {
+                    var targetCell = newWorksheet.Cells[cell.Start.Row, cell.Start.Column];
+                    targetCell.Value = cell.Text;
+
+                    // Copia el estilo de la celda (incluyendo colores)
+                    targetCell.Style.Numberformat.Format = cell.Style.Numberformat.Format;
+                    targetCell.Style.Font.Bold = cell.Style.Font.Bold;
+                    targetCell.Style.Font.Italic = cell.Style.Font.Italic;
+                    targetCell.Style.Font.Size = cell.Style.Font.Size;
+                    targetCell.Style.HorizontalAlignment = cell.Style.HorizontalAlignment;
+                    targetCell.Style.VerticalAlignment = cell.Style.VerticalAlignment;
+
+                    // Copia los bordes de la celda (si existen)
+                    if (cell.Style.Border.Left.Style != ExcelBorderStyle.None)
+                    {
+                        targetCell.Style.Border.Left.Style = cell.Style.Border.Left.Style;
+                        targetCell.Style.Border.Left.Color.SetColor(ColorTranslator.FromHtml(cell.Style.Border.Left.Color.Rgb));
+                    }
+                    if (cell.Style.Border.Right.Style != ExcelBorderStyle.None)
+                    {
+                        targetCell.Style.Border.Right.Style = cell.Style.Border.Right.Style;
+                        targetCell.Style.Border.Right.Color.SetColor(ColorTranslator.FromHtml(cell.Style.Border.Right.Color.Rgb));
+                    }
+                    if (cell.Style.Border.Top.Style != ExcelBorderStyle.None)
+                    {
+                        targetCell.Style.Border.Top.Style = cell.Style.Border.Top.Style;
+                        targetCell.Style.Border.Top.Color.SetColor(ColorTranslator.FromHtml(cell.Style.Border.Top.Color.Rgb));
+                    }
+                    if (cell.Style.Border.Bottom.Style != ExcelBorderStyle.None)
+                    {
+                        targetCell.Style.Border.Bottom.Style = cell.Style.Border.Bottom.Style;
+                        targetCell.Style.Border.Bottom.Color.SetColor(ColorTranslator.FromHtml(cell.Style.Border.Bottom.Color.Rgb));
+                    }
+
+                    targetCell.AutoFitColumns();
+                }
+
+                var texto1 = new List<string>() { "M", "N", "O", "P", "Q" };
+                var num1 = new List<int>() { 3, 4, 5, 6, 7 };
+                var texto2 = new List<string>() { "U", "V", "W", "X", "Y" };
+                var num2 = new List<int>() {  11, 12, 13 ,14, 15};
+                var texto3 = new List<string>() { "Q", "R", "S", "T", "U" };
+                var num3 = new List<int> { 19, 20, 21, 22,23 };
+
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if(carton[i][j] != "libre")
+                        {
+                            var te = $"{carton[i][j]:D2}".ToString();
+
+                            newWorksheet.Cells[$"{texto1[j]}{num1[i]}"].Value = te;
+                            newWorksheet.Cells[$"{texto1[j]}{num1[i]}"].Style.Numberformat.Format = "@";
+                            newWorksheet.Cells[$"{texto1[j]}{num3[i]}"].Value = te; ;
+                            newWorksheet.Cells[$"{texto1[j]}{num3[i]}"].Style.Numberformat.Format = "@";
+                            newWorksheet.Cells[$"{texto3[j]}{num2[i]}"].Value = te; ;
+                            newWorksheet.Cells[$"{texto3[j]}{num2[i]}"].Style.Numberformat.Format = "@";
+                            newWorksheet.Cells[$"{texto2[j]}{num1[i]}"].Value = te; ;
+                            newWorksheet.Cells[$"{texto2[j]}{num1[i]}"].Style.Numberformat.Format = "@";
+                            newWorksheet.Cells[$"{texto2[j]}{num3[i]}"].Value = te; ;
+                            newWorksheet.Cells[$"{texto2[j]}{num3[i]}"].Style.Numberformat.Format = "@";
+                        }
+
+                    }
+
+                }
+                newWorksheet.Cells[$"Q8"].LoadFromText($"Cartón{n:D3}");
+                newWorksheet.Cells[$"Q8"].AutoFitColumns();
+
+                newWorksheet.Cells[$"U16"].LoadFromText($"Cartón{n:D3}");
+                newWorksheet.Cells[$"U16"].AutoFitColumns();
+
+                newWorksheet.Cells[$"Y8"].LoadFromText($"Cartón{n:D3}");
+                newWorksheet.Cells[$"Y8"].AutoFitColumns();
+
+                newWorksheet.Cells[$"Q24"].LoadFromText($"Cartón{n:D3}");
+                newWorksheet.Cells[$"Q24"].AutoFitColumns();
+
+                newWorksheet.Cells[$"Y24"].LoadFromText($"Cartón{n:D3}");
+                newWorksheet.Cells[$"Y24"].AutoFitColumns();
+
+                // Guarda el archivo Excel con la hoja copiada
+                package.Save();
+            }
+
+        }
+
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿using GeneradorBingo;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Random random = new Random();
+    static List<string[][]> cartonesGenerados = new List<string[][]>();
+    static void Main()
+    {
+        int totalCartones = 300;
+        int filas = 5;
+        int columnas = 5;
+
+
+
+        for (int i = 0; i < totalCartones; i++)
+        {
+            string[][] carton;
+            bool esCartonUnico;
+            bool filaRepetida = false;
+            do
+            {
+                carton = GenerarCarton(filas, columnas);
+                esCartonUnico = EsCartonUnico(carton, cartonesGenerados);
+                filaRepetida=  FilasRepetidas(carton);
+
+                if (filaRepetida)
+                {
+                    Console.WriteLine("Carton con fila repetida");
+                    ImprimirCarton(carton, filas, columnas);
+                }
+                if (!esCartonUnico)
+                {
+                    Console.WriteLine("Carton repetido");
+                    ImprimirCarton(carton, filas, columnas);
+                }
+            } while (!esCartonUnico || filaRepetida);
+
+            cartonesGenerados.Add(carton);
+            
+            Exportar.CrearArchivo(carton, i + 1);
+
+            Console.WriteLine($"Cartón {i + 1}:");
+            ImprimirCarton(carton, filas, columnas);
+            Console.WriteLine();
+        }
+    }
+
+
+    static bool FilasRepetidas(string[][] carton)
+    {
+        if (cartonesGenerados.Count() == 0) return false;
+        
+        List<int> filasACuidar = new List<int>() { 0,4};
+
+        foreach (var i in filasACuidar)
+        {
+            var matrizFila = carton[i].ToList();
+
+            foreach(var item in cartonesGenerados)
+            {
+                var fila = item[i].ToList();
+
+                var coincidencia = fila.Intersect(matrizFila).ToList();
+                if (coincidencia.Count() > 4 ) 
+                {
+                    Console.WriteLine("Se repite");
+                    
+                    return true;
+                } 
+            }
+
+        }
+        return false;
+    }
+
+    static bool ColumnasRepetida(string[][] carton)
+    {
+        if (cartonesGenerados.Count() == 0) return false;
+
+        for (int i = 0; i < 5; i++)
+        {
+            var matrizColumna = GetColumna(carton, i);
+
+            var repite = cartonesGenerados
+                .FirstOrDefault(n => GetColumna(n, i) == matrizColumna);
+
+            if (repite != null) return true;
+        }
+
+
+        return false;
+    }
+
+    static string[] GetColumna(string[][] carton, int columna)
+    {
+        string[] matriz = new string[5];
+
+        for (int fila = 0; fila < matriz.Length; fila++)
+        {
+            matriz[fila] = carton[fila][columna];
+        }
+        return matriz;
+    }
+    static string[][] GenerarCarton(int filas, int columnas)
+    {
+        string[][] carton = new string[filas][];
+
+        for (int i = 0; i < filas; i++)
+        {
+            carton[i] = new string[columnas];
+
+            for (int j = 0; j < columnas; j++)
+            {
+                if (i == 2 && j == 2)
+                {
+                    // Espacio libre en el centro
+                    carton[i][j] = "libre";
+                }
+                else
+                {
+                    string letra = string.Empty;
+                    int minNumero = j * 15 + 1;
+                    int maxNumero = minNumero + 14;
+
+                    int numero;
+                    do
+                    {
+                        numero = random.Next(minNumero, maxNumero + 1);
+                    } while (NumeroRepetido(carton, numero, j));
+
+                    carton[i][j] = $"{letra}{numero:D2}";
+                }
+            }
+        }
+
+        return carton;
+    }
+
+    static bool NumeroRepetido(string[][] carton, int numero, int columna)
+    {
+        foreach (var fila in carton)
+        {
+            if (fila != null && fila[columna] != null && fila[columna].EndsWith(numero.ToString("D2")))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static bool EsCartonUnico(string[][] carton, List<string[][]> cartonesGenerados)
+    {
+        foreach (var cartonGenerado in cartonesGenerados)
+        {
+            if (SonCartonesIguales(carton, cartonGenerado))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static bool SonCartonesIguales(string[][] carton1, string[][] carton2)
+    {
+        var cartonLineal1 = new List<int>();
+        var cartonLineal2 = new List<int>();
+        foreach (var item in carton1)
+        {
+            cartonLineal1 = cartonLineal1.Concat(item.ToList().Select(x=> Convert.ToInt32(x=="libre" ? "0":x))).ToList();
+        }
+
+        foreach (var item in carton2)
+        {
+            cartonLineal2 = cartonLineal2.Concat(item.ToList().Select(x => Convert.ToInt32(x == "libre" ? "0":x))).ToList();
+        }
+
+
+
+        //Console.WriteLine("Coincidencia " + coincidencias);
+       var repitencia = cartonLineal1.Intersect(cartonLineal2).Count();
+
+        if(repitencia > 15)
+        {
+            Console.WriteLine($"Comparten {repitencia} letras");
+        }
+
+        return repitencia > 23;
+    }
+
+    static void ImprimirCarton(string[][] carton, int filas, int columnas)
+    {
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(carton[i][j].PadLeft(6));
+            }
+            Console.WriteLine();
+        }
+    }
+}
